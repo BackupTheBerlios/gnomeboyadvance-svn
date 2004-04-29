b116610d7028cc392716647da864f67eef8aa6fb
twolife <twolife@a8647284-77d7-0310-a28f-d6253c6586bb>
2004-04-29 20:49:30 +0000

    completely useless

--- install.py
+++ install.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 import os
 
-installDir = '/usr/bin'
-dataDir = '/usr/share/gnomeboyadvance'
+prefix = '/usr/'
 
 if not os.path.exists(dataDir):
 	print 'making ' + dataDir
@@ -12,8 +11,10 @@ if not os.path.exists(installDir):
 	# really!
 	os.mkdir(installDir)
 
-os.system('cp src/gnomeboyadvance.glade ' + dataDir)
-os.system('cp pixmaps/gnomeBoyAdvance.png ' + dataDir)
-os.system('cp src/gnomeboyadvance ' + installDir)
-os.system('cp pixmaps/gba48.png ' + dataDir)
+os.system('install -m 755 src/gnomeboyadvance ' + prefix + 'bin/')
+os.system('install -m 644 src/gnomeboyadvance.glade ' + prefix + 'share/gnomeboyadvance/')
+os.system('install -m 644 data/gba48.png ' + prefix + 'share/pixmaps/')
+os.system('install -m 644 data/gnomeboyadvance.application ' + prefix + 'share/applications/')
+os.system('install -m 644 data/gnomeboyadvance.png ' + prefix + 'share/gnomeboyadvance/')
+
 print 'gnomeboyadvance has been installed, happy gaming.'
