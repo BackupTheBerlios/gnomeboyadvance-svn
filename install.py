3138a07d5cdc509bf2cc8b51dc606a300e9140a0
twolife <twolife@a8647284-77d7-0310-a28f-d6253c6586bb>
2004-04-29 21:09:02 +0000

    official boulay(tm)

--- install.py
+++ install.py
@@ -14,7 +14,7 @@ if not os.path.exists(installDir):
 os.system('install -m 755 src/gnomeboyadvance ' + prefix + 'bin/')
 os.system('install -m 644 src/gnomeboyadvance.glade ' + prefix + 'share/gnomeboyadvance/')
 os.system('install -m 644 data/gba48.png ' + prefix + 'share/pixmaps/')
-os.system('install -m 644 data/gnomeboyadvance.application ' + prefix + 'share/applications/')
+os.system('install -m 644 data/gnomeboyadvance.desktop ' + prefix + 'share/applications/')
 os.system('install -m 644 data/gnomeboyadvance.png ' + prefix + 'share/gnomeboyadvance/')
 
 print 'gnomeboyadvance has been installed, happy gaming.'
