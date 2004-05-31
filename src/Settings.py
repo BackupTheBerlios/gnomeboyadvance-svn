88779b9d5cb2c923b27081eab6bf49f3c7021516
cassidy <cassidy@a8647284-77d7-0310-a28f-d6253c6586bb>
2004-05-31 22:01:40 +0000

    add check and exception

--- src/Settings.py
+++ src/Settings.py
@@ -13,20 +13,20 @@ ADVANCED_OPTIONS = ['saveType', 'biosFile', 'flashSize', 'emulatorType', 'showSp
 class Settings:
 	def __init__(self):
 		self.settings = {}
-		#humm better to make that when we open the preferences box AND binary == ''
 
 		self.client = gconf.client_get_default();
 		self.client.add_dir("/apps/gnomeboyadvance", gconf.CLIENT_PRELOAD_NONE)
 
 		self.readGConf()
-		if not self.settings['binary']:
-			bin = os.popen('which VisualBoyAdvance').read()
-			self.settings['binary'] = bin[:-1]
+		if not self.settings['binary']: self.tryFindBinary
 	
 		login = pwd.getpwuid(os.geteuid())[0]
 		self.path = os.path.join('/tmp', 'VisualBoyAdvance_' + login + '.cfg')
 		self.validity = False
-
+	
+	def tryFindBinary(self):
+		bin = os.popen('which VisualBoyAdvance').read()
+		if os.access(bin[:-1], os.X_OK ): self.settings['binary'] = bin[:-1]
 
 	def readGConf(self):
 		for elt in GENERAL_OPTIONS:
@@ -77,6 +77,10 @@ class Settings:
 		f.close()
 
 	def readFile(self, path):
+		if not os.access(path, os.R_OK): 
+			msg = "Unable to open " + path
+			raise "GBAError", msg
+
 		buff = file(path, 'r').readlines()
 		#regexp = re.compile("[=\t]")
 
@@ -91,9 +95,10 @@ class Settings:
 			else:
 				tmp[1] = tmp[1][:-1]	#drop \n (i know it's dirty!)
 			
-			#if not self.settings.has_key(tmp[0]):
-			#	print "FUCK OFF:", tmp[0] #TODO: raise a exception
-			#	sys.exit()
+			if not self.settings.has_key(tmp[0]):
+				msg = tmp[0] + " is not a valid settings item"
+				raise "GBAError", msg
+
 			if len(tmp) != 2: 
 				print "droped", tmp
 				continue
@@ -102,6 +107,28 @@ class Settings:
 	def confFile(self):
 		if not self.validity: self.writeFile(self.path)
 		return self.path
+
+	def check(self):
+		#binary
+		if not os.access(self.settings['binary'], os.X_OK ): 
+			msg = self.settings['binary'] + ": Invalide path to VBA or not an executable"
+			return msg
+
+		#romsDir
+		dir = self.settings['romsDir']
+		if not os.path.isdir(dir) or not os.access(dir, os.R_OK):
+			msg = dir + ": is not a readable directory"
+			return msg
+
+		#datas dirs
+		for dir_name in ['captureDir', 'saveDir', 'batteryDir']:
+			dir = self.settings[dir_name]
+			if not dir: continue #skip if no value
+			if not os.path.isdir(dir) or not os.access(dir, os.W_OK):
+				msg = dir + ": is not a writable directory"
+				return msg
+
+		return None
 
 if __name__ == "__main__":
 	#self test
