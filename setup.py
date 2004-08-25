922c683363301ce2b077d488f4a4e5ec0459ca12
cassidy <cassidy@a8647284-77d7-0310-a28f-d6253c6586bb>
2004-08-25 19:36:26 +0000

    distutils usable

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@
 
 '''
 
+import sys
+import os
 from distutils.core import setup
 
 from src.const import VERSION, COMMENTS
 
-DISTUTILS_DEBUG = 1	#TODO: remove for release
+#DISTUTILS_DEBUG = 1
 
 setup(	name		= 'gnomeboyadvance',
       	version		= VERSION,
@@ -20,11 +22,19 @@ setup(	name		= 'gnomeboyadvance',
 	url		= 'http://developer.berlios.de/projects/gnomeboyadvance/',
 	license		= 'GPL',
 	data_files	= [	('share/gnomeboyadvance', ['glade/gnomeboyadvance.glade', 'data/gnomeboyadvance.png']),
-      				('share/applications', ['data/gnomeboyadvance.desktop']),
-				('share/pixmaps', ['data/gba48.png']),
-				('share/gconf/schemas', ['data/gnomeboyadvance.schemas'])
+      		('share/applications', ['data/gnomeboyadvance.desktop']),
+		('share/pixmaps', ['data/gba48.png']),
+		('share/gconf/schemas', ['data/gnomeboyadvance.schemas'])
 			],
       	packages	= ['gnomeboyadvance'],
 	package_dir	= {'gnomeboyadvance' : 'src'},
 	scripts		= ['gnomeboyadvance']
-      )
+	)
+
+#VERY bad but the doc of distutils sux!
+#TODO: make this clean
+try:
+	if sys.argv[1] == "install": 
+		os.system('GCONF_CONFIG_SOURCE=`gconftool-2 --get-default-source` gconftool-2 --makefile-install-rule data/gnomeboyadvance.schemas')
+	
+except IndexError: pass
